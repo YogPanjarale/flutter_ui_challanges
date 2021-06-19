4685bc5aaea10d75cefb58eb92e5e153cebcdca4
YogPanjarale <yog.panjarale@gmail.com>
2021-06-20 00:02:03 +0530

    ui day one done

--- lib/day/day_1.dart
+++ lib/day/day_1.dart
@@ -7,26 +7,29 @@ class Day1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration:   BoxDecoration(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [Colors.blueAccent.shade400, Colors.lightBlue],
             ),
           ),
-          child: Column(
-            children: [
-              Row(
-                children: const [
-                  Icon(
-                    Icons.keyboard_arrow_left,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 50,),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                child: Column(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.keyboard_arrow_left,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                //Text
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: const [
                     Text(
@@ -47,9 +50,140 @@ class Day1 extends StatelessWidget {
                     ),
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white.withOpacity(0.55),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 24, horizontal: 16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        TextFormField(
+                          initialValue: 'Yog Panjarale',
+                          decoration: const InputDecoration(
+                            labelText: 'Name',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          initialValue: 'yogpanjarale@gmail.com',
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          initialValue: '123456',
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 10),
+                            child: Text(
+                              "Sign Up!",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Center(
+                          child: Text(
+                            "—————— or ——————",
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const MyIcon(
+                                url:
+                                    "https://pngimg.com/uploads/google/google_PNG19635.png",
+                              ),
+                              Image.network(
+                                "https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19754.png",
+                                height: 60,
+                                width: 60,
+                                fit: BoxFit.cover,
+                              ),
+                              const MyIcon(
+                                  url:
+                                      "https://pngimg.com/uploads/twitter/twitter_PNG95259.png")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           )),
+    );
+  }
+}
+
+class MyIcon extends StatelessWidget {
+  const MyIcon({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+  final String url;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
+      child: Image.network(
+        url,
+        height: 30,
+        width: 30,
+      ),
     );
   }
 }
